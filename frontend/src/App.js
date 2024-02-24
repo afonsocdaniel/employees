5fd0c7515fc5526d371db3d70d0ce5cb57f29a0d
Daniel Filho <daniel.acfilho@gmail.com>
2024-02-24 00:08:28 -0300

    feat(App): configure react-query

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,24 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient } from '@tanstack/react-query';
+
 import EmployeesListPage from "./pages/EmployeesListPage";
 import EmployeeDetailspage from "./pages/EmployeeDetailspage";
 
+const queryClient = new QueryClient({});
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<EmployeesListPage />} />
-        <Route path="employees/:id" element={<EmployeeDetailspage />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<EmployeesListPage />} />
+          <Route path="employees/:id" element={<EmployeeDetailspage />} />
+        </Routes>
+      </BrowserRouter>
+
+    </QueryClientProvider>
   );
 };
 
