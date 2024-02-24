ba4a371b285a68e8b8c255555976097b79b16051
Daniel Filho <daniel.acfilho@gmail.com>
2024-02-23 21:44:30 -0300

    feat(pages/employeesListPage): fetch data from the API and render a card for each employee (#4)
    * fix(back-end): fix cors headers
    
    * feat(pages/employeesListPage): fetch date from the back and render each card

--- frontend/src/pages/EmployeesListPage.js
+++ frontend/src/pages/EmployeesListPage.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import { Layout, theme } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Layout, Card, Col, Row, theme } from 'antd';
 
 const { Header, Content } = Layout;
+const { Meta } = Card;
 
 const EmployeesListPage = () => {
-  const { token: { colorBgContainer, borderRadiusLG }, } = theme.useToken();
+  const { token: { colorBgContainer }, } = theme.useToken();
+  const [employees, setEmployees] = useState([]);
+  const loadEmployees = async () => {
+    const res = await fetch("http://localhost:5001/api/employees.json?page=1&per_page=8");
+    const json = await res.json();
+    setEmployees(json.employees);
+  };
+
+  useEffect(() => {
+    loadEmployees();
+  }, []);
 
   return (
     <Layout>
-      <Header style={{ padding: 0, background: colorBgContainer }} />
+      <Header style={{ background: colorBgContainer }} />
 
       <Content style={{ margin: '24px 16px 0' }} >
-        <div
-          style={{
-            padding: 24,
-            minHeight: 360,
-            background: colorBgContainer,
-            borderRadius: borderRadiusLG,
-          }}
-        >
-          content
-        </div>
+        <Row gutter={[16, 16]}>
+          {employees.map((employee) => (
+            <Col span={6} key={employee.id}>
+              <Card
+                onClick={() => { console.log(employee.id) }}
+                hoverable
+                cover={<img alt={employee.first_name} src={employee.avatar} />}
+              >
+                <Meta
+                  title={`${employee.first_name} ${employee.last_name}`}
+                  description={employee.email}
+                />
+              </Card>
+            </Col>
+          ))}
+        </Row>
+
       </Content>
     </Layout>
   );
