b5c9f5a40bc839fd6e3566a40d123fd91118d50f
Daniel Filho <daniel.acfilho@gmail.com>
2024-02-23 22:53:38 -0300

    feat(front-end): Add employee details page (#6)
    * feat(pages/EmployeeDetailsPage): implement the details page with the employee info
    
    * feat(EmployeeDetailspage): add not found state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import EmployeesListPage from "./pages/EmployeesListPage";
+import EmployeeDetailspage from "./pages/EmployeeDetailspage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<EmployeesListPage />} />
+        <Route path="employees/:id" element={<EmployeeDetailspage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/EmployeeDetailspage.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import { Layout, Breadcrumb, Avatar, Col, Row, Divider, Descriptions, Result, Button, theme } from 'antd';
+
+const { Header, Content } = Layout;
+
+const EmployeeNotFound = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Result
+      status="404"
+      title="Employee found"
+      subTitle="Sorry, we couldn't find the employee you were looking for."
+      extra={<Button type="primary" onClick={() => navigate('/')}>Back Home</Button>}
+    />
+  );
+}
+
+const EmployeeDetailspage = () => {
+  const { token: { colorBgContainer }, } = theme.useToken();
+  const params = useParams();
+  const [employeeInfo, setEmployeeInfo] = useState({});
+
+  useEffect(() => {
+    const loadEmployeeData = async () => {
+      const res = await fetch(`http://localhost:5001/api/employees/${params.id}.json`);
+
+      if (res.status !== 200) { return setEmployeeInfo(null); };
+
+      const json = await res.json();
+      setEmployeeInfo(json.employee);
+    };
+
+    loadEmployeeData();
+  }, [params.id]);
+
+  if (!employeeInfo) { return <EmployeeNotFound />; }
+
+  return (
+    <Layout>
+      <Header style={{ background: colorBgContainer }} />
+
+      <Content>
+        <Breadcrumb
+          separator=">"
+          items={[ { title: 'Employees', href: '/', }, { title: employeeInfo.first_name }]}
+        />
+
+        <Divider />
+
+        <Row gutter={[16, 16]}>
+          <Col span={16}>
+            <Descriptions title="Employee Info" bordered size={'small'}>
+              <Descriptions.Item label="Profile Picture"><Avatar size={64} src={employeeInfo.avatar}/></Descriptions.Item>
+              <Descriptions.Item label="First Name">{employeeInfo.first_name}</Descriptions.Item>
+              <Descriptions.Item label="Last Name">{employeeInfo.last_name}</Descriptions.Item>
+              <Descriptions.Item label="Email">{employeeInfo.email}</Descriptions.Item>
+              <Descriptions.Item label="ID">#{employeeInfo.id}</Descriptions.Item>
+            </Descriptions>
+          </Col>
+        </Row>
+      </Content>
+    </Layout>
+  );
+};
+
+export default EmployeeDetailspage;
